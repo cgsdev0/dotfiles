812a11ffdd78669e85d303fc34537bdb87f08922
Shane Schulte <shane.schulte@samsara.com>
2019-05-10 19:32:56 -0700

    installation tweak

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
