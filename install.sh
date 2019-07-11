ab6896b6ca8553df1e08a95edd28c30d97320b8a
Shane Schulte <schulte.shane@gmail.com>
2019-07-10 22:38:00 -0700

    un-hardcode install repo path

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
 cat directories | xargs -L1 sudo mkdir -p
-cat files | xargs -L1 -I {} ln -sf /home/$USER/dotfiles/{} /home/$USER/{}
+cat files | xargs -L1 -I {} ln -sf $SCRIPT_DIR/{} /home/$USER/{}
