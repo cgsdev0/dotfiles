78ce739933c17fc526ed8843ca49256a85a7e072
Shane Schulte <shane.schulte@samsara.com>
2019-10-29 18:51:56 +0000

    add other programs to setup

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
 cat directories | xargs -L1 -I {} sudo mkdir -p /home/$USER/{}
 cat files | xargs -L1 -I {} sudo ln -sf $SCRIPT_DIR/{} /home/$USER/{}
+
+sudo ln -sf /home/$USER/.profile /home/$USER/.zprofile
--- setup.sh
+++ setup.sh
@@ -1,5 +1,64 @@
 #!/bin/bash
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+./install.sh
+mkdir -p ~/Tools
+cd ~/Tools
+
+# Install zsh plugins
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions already installed"
+fi
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search ]; then
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+else
+    echo "zsh-history-substring-search already installed"
+fi
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting already installed"
+fi
+
+
+# Install neovim 0.4.2
+if ! vim -v | grep "NVIM v0.4.2" -q; then
+    wget https://github.com/neovim/neovim/releases/download/v0.4.2/nvim.appimage
+    sudo mv nvim.appimage ~/bin/.
+    sudo update-alternatives --install /usr/bin/vim vim ~/bin/nvim.appimage 200
+    chmod +x nvim.appimage
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo "NVIM v0.4.2 already installed"
+fi
+
+# install global nodejs
+if ! which node > /dev/null; then
+    sudo apt-get install nodejs
+else
+    echo "nodejs already installed"
+fi
+
+# install global yarn
+if ! which yarn > /dev/null; then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list\n
+    sudo apt update
+    sudo apt install yarn
+else
+    echo "yarn already installed"
+fi
+
+# Install fzf
+if ! which fzf > /dev/null; then
+    cd ~/Tools
+    wget https://github.com/junegunn/fzf/archive/0.18.0.tar.gz
+    mkdir -p fzf
+    tar -xzf 0.18.0.tar.gz -C fzf
+    cd fzf/fzf-0.18.0
+    ./install
+else
+    echo "fzf already installed"
+fi
