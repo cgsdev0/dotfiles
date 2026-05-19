b957cb4bf26e891dfdfa424946a1ce5865490dfd
Sarah Schulte <git@sarah.engineer>
2026-05-19 17:09:42 -0400

    jank: laptop commit

--- .zshrc
+++ .zshrc
@@ -204,8 +204,6 @@ fi
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-. "$HOME/.local/bin/env"
-
 # fnm
 FNM_PATH="/home/badcop/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
--- _setup.sh
+++ _setup.sh
@@ -1,21 +1,4 @@
-#!/bin/bash
-
-
-sudo apt-get update
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	sudo apt-get install -y build-essential \
-      fzf \
-      fd-find \
-      ripgrep \
-      zsh \
-      unzip \
-      curl \
-      direnv \
-      powerline \
-      clang-format \
-      tmux
-fi
+#!/usr/bin/env bash
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -23,9 +6,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 ./_install.sh
 mkdir -p ~/tools
 mkdir -p ~/todo
-
-sudo chown -R sarah:sarah ~/
-cd ~/tools
 
 # Install zsh plugins
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
@@ -55,35 +35,3 @@ else
     echo "pure already installed"
 fi
 
-# Install neovim
-if ! vim --version | grep "NVIM" -q; then
-    cd ~/tools
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	sudo add-apt-repository ppa:neovim-ppa/unstable
-	sudo apt-get update
-	sudo apt-get install -y neovim
-	sudo update-alternatives --install /usr/bin/vim vim $(which nvim) 200
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-	NEOVIM_VERSION="0.6.0"
-        wget https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim-macos.tar.gz
-        tar -xzf nvim-macos.tar.gz
-        rm nvim-macos.tar.gz
-        sudo ln -sf $HOME/tools/nvim-osx64/bin/nvim $HOME/bin/nvim
-    fi
-else
-    echo "NVIM $NEOVIM_VERSION already installed"
-fi
-
-# Install baton (Spotify CLI)
-BATON_VERSION="0.1.7"
-if ! which baton > /dev/null; then
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        cd ~/tools
-        curl -LO https://github.com/joshuathompson/baton/releases/download/$BATON_VERSION/baton-${BATON_VERSION}-linux-amd64
-        mv baton-${BATON_VERSION}-linux-amd64 "$HOME/bin/baton"
-        sudo chmod +x "$HOME/bin/baton"
-        ~/bin/baton auth
-    fi
-else
-    echo "baton already installed"
-fi
