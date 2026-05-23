6e6e8c9e96133801c03f6d5386329a34b8547dae
Sarah Schulte <git@sarah.engineer>
2026-05-22 20:26:35 -0400

    yup

--- _nixos/configuration.nix
+++ _nixos/configuration.nix
@@ -92,6 +92,8 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     wl-clipboard
+    ripgrep
+    fd
     mako
     tmux
     discord-ptb
@@ -111,6 +113,10 @@
     zulip
     gcc
     gnumake
+    nodejs
+    godot
+    wayland-scanner
+    s3cmd
   ];
   services.gnome.gnome-keyring.enable = true;
 
